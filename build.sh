d4777e2d4b52ca53306e0273a7efbfbde325f29c
MeDeveloperWeb <www.shivachaurasia@gmail.com>
2022-11-02 23:27:55 +0530

    a

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 poetry install
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+pip install -r requirements.txt
+python pustak/manage.py collectstatic --no-input
+python pustak/manage.py migrate
