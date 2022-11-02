f4ff7018b251356343b8ab629fa4f2a86beaed76
MeDeveloperWeb <www.shivachaurasia@gmail.com>
2022-11-02 23:30:52 +0530

    render

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 poetry install
-pip install -r requirements.txt
+pip install -r pustak/requirements.txt
 python pustak/manage.py collectstatic --no-input
 python pustak/manage.py migrate
